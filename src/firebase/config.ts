f7ceb72b4abd6b8f0b2fa1a1b4d0ac0b5797a455
Mubasir MR <mubasir@dev-bee.com>
2025-10-01 17:04:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5369376882-6584f",
+  "appId": "1:900055197803:web:2498379e20be4aa0e8292e",
+  "apiKey": "AIzaSyDjj2Vps4W8Rr2jczphhow8rJkupeTLQyY",
+  "authDomain": "studio-5369376882-6584f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "900055197803"
+};
